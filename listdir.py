67f3935ed4edb1039c05fab0060294e2b8bcecee
J0hnZMT <tuddaojohnzel@gmail.com>
2019-10-16 10:44:39 +0800

    date and time functionality added

--- listdir.py
+++ listdir.py
@@ -11,12 +11,37 @@ import csv
 import hashlib
 from zipfile import ZipFile
 import configparser
+from datetime import datetime
+
+
+def md5_hash(file_to_hash):
+    block_size = 65536
+    hash_md5 = hashlib.md5()
+    # getting the md5 hashes of files
+    with open(file_to_hash, 'rb') as hash_file:
+        buf = hash_file.read(block_size)
+        while len(buf) > 0:
+            hash_md5.update(buf)
+            buf = hash_file.read(block_size)
+    return hash_md5.hexdigest()
+
+
+def sha1_hash(file):
+    block_size = 65536
+    hash_sha1 = hashlib.sha1()
+    # getting the sha1 hashes of files
+    with open(file, 'rb') as hash_file:
+        buf = hash_file.read(block_size)
+        while len(buf) > 0:
+            hash_sha1.update(buf)
+            buf = hash_file.read(block_size)
+    return hash_sha1.hexdigest()
 
 
 def csv_archive(zip_file_name):
     # writing files to a zipfile
     os.chdir("C:\\Users\\TEU_USER\\Documents\\python-exer\\listdir")
-    zip_name = '{}.zip'.format(zip_file_name)
+    zip_name = "{}.zip".format(zip_file_name)
     with ZipFile(zip_name, 'w') as file_to_zip:
         file_to_zip.write(zip_file_name)
     # deleting the csv file
@@ -24,19 +49,23 @@ def csv_archive(zip_file_name):
     print('{}.zip created...'.format(zip_file_name))
 
 
-def finder(dir_name, csv_file_name):
-    # find the directory of the path given
+def add_date_time(csv_file):
+    # add date and time to the output
+    now = datetime.now()
+    datetime_now = now.strftime("%m-%d-%y %I-%M-%p")
+    csv_filename_now = "{} {}.csv".format(csv_file, datetime_now)
+    return csv_filename_now
+
+
+def list_dir(dir_name, csv_file_name):
     find_path = os.path.exists(dir_name)
-    # if the path is not in the list of directories
     if not find_path:
         print("Path not found!")
     else:
         try:
-            block_size = 65536
-            hash_md5 = hashlib.md5()
-            hash_sha1 = hashlib.sha1()
             # create a csv file
-            with open(csv_file_name, "w+", newline='') as csv_file:
+            csv_name = add_date_time(csv_file_name)
+            with open(csv_name, "w+", newline='') as csv_file:
                 # header for the csv file
                 field_header = ['Parent Name', 'File Name', 'File Size', 'MD5', 'SHA-1']
                 csv_writer = csv.DictWriter(csv_file, fieldnames=field_header)
@@ -47,21 +76,15 @@ def finder(dir_name, csv_file_name):
                     for file_name in file_names:
                         os.chdir(dir_path)
                         file_size = os.path.getsize(os.path.join(dir_path, file_name))
-                        # getting the md5 and sha1 hashes of files
-                        with open(file_name, 'rb') as hash_file:
-                            buf = hash_file.read(block_size)
-                            while len(buf) > 0:
-                                hash_md5.update(buf)
-                                hash_sha1.update(buf)
-                                buf= hash_file.read(block_size)
-                        md5_hash = hash_md5.hexdigest()
-                        sha1_hash = hash_sha1.hexdigest()
+                        # get the hashes of the files
+                        md5_file_hash = md5_hash(file_name)
+                        sha1_file_hash = sha1_hash(file_name)
                         # store the data in a dictionary
-                        report = {'Parent Name': str(dir_path), 'File Name': file_name, 'File Size': file_size, 'MD5': md5_hash, 'SHA-1': sha1_hash}
+                        report = {'Parent Name': str(dir_path), 'File Name': file_name, 'File Size': file_size, 'MD5': md5_file_hash, 'SHA-1': sha1_file_hash}
                         # store the data by row in the csv file
                         csv_writer.writerow(report)
             # archive the csv output file
-            csv_archive(csv_file_name)
+            csv_archive(csv_name)
         except OSError as e:
             # catch any error and display
             print("Ops, Something went wrong! {}".format(e))
@@ -78,9 +101,9 @@ def main():
     if args.dir is None and args.csv_file_name is None:
         config.read('setup.ini')
         # using the default arguments from setup.ini
-        finder(config['arguments']['file path'], config['arguments']['file name'])
+        list_dir(config['arguments']['file path'], config['arguments']['file name'])
     else:
-        finder(args.dir, args.csv_file_name)
+        list_dir(args.dir, args.csv_file_name)
 
 
 if __name__ == '__main__':
