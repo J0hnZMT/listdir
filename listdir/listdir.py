2c236d7b50511ed16eec39a6d9d438126bb26518
J0hnZMT <tuddaojohnzel@gmail.com>
2019-11-04 16:40:25 +0800

    added save to db feature

--- listdir/listdir.py
+++ listdir/listdir.py
@@ -15,8 +15,24 @@ from datetime import datetime
 import yaml
 import logging.config
 import logging
-import coloredlogs
 import json
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+import getpass
+from configparser import ConfigParser
+
+
+class Password:
+
+    DEFAULT = 'Prompt if not specified'
+
+    def __init__(self, passwd):
+        if passwd == self.DEFAULT:
+            passwd = getpass.getpass('Database Password: ')
+        self.passwd = passwd
+
+    def __str__(self):
+        return self.passwd
 
 
 def setup_logging(default_path, default_level, env_key):
@@ -30,16 +46,13 @@ def setup_logging(default_path, default_level, env_key):
             try:
                 config = yaml.safe_load(f.read())
                 logging.config.dictConfig(config)
-                coloredlogs.install()
             except Exception as e:
                 print(e)
                 print('Error in Logging Configuration. Using default configs')
                 logging.basicConfig(level=default_level)
-                coloredlogs.install(level=default_level)
     else:
         logging.basicConfig(level=default_level, filename='logs.log',
                             format="%(asctime)s:%(name)s:%(levelname)s:%(message)s")
-        coloredlogs.install(level=default_level)
         print('Failed to load configuration file. Using default configs')
 
 
@@ -50,6 +63,118 @@ env = 'LOG_CFG'
 setup_logging(path, level, env)
 logger = logging.getLogger(__name__)
 logger.info("logger set..")
+
+
+def config_open(filename='setup.ini', section='database'):
+    # create a parser
+    parser = ConfigParser()
+    # read config file
+    parser.read(filename)
+
+    # get section, default to postgresql
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+
+    return db
+
+
+def db_check(dir_name, db_pw):
+    conn = None
+    # logger.info(conn.get_dsn_parameters())
+    try:
+        params = config_open()
+        conn = psycopg2.connect(**params, password=db_pw)
+        conn.autocommit = True
+        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        cur = conn.cursor()
+        # checking if database exist
+        cur.execute("select exists(SELECT datname FROM pg_catalog.pg_database WHERE datname = 'listdirdb');")
+        if cur.fetchone()[0]:
+            logger.info("Database exist... continue with data insertion.")
+            # looking if table exist
+            table_check(dir_name, db_pw)
+        else:
+            logger.info("Creating Database listdirdb....")
+            database = """CREATE DATABASE listdirdb;"""
+            cur.execute(database)
+            cur.close()
+            conn.close()
+            table_check(dir_name, db_pw)
+    except (Exception, psycopg2.DatabaseError) as error:
+        logger.exception(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+def db_insert(dir_name, db_pw):
+    conn = None
+    try:
+        params = config_open()
+        conn = psycopg2.connect(**params,
+                                password=db_pw,
+                                database='listdirdb')
+        conn.autocommit = True
+        cur = conn.cursor()
+        insert_query = """INSERT INTO public.result(parent_name, file_name, file_size, md5, sha1) VALUES (%s, %s, %s, %s, %s);"""
+        for dir_path, dir_names, file_names in os.walk(dir_name):
+            for file_name in file_names:
+                file_with_path = "{}{}{}".format(dir_path, os.sep, file_name)
+                file_size = os.path.getsize(file_with_path)
+                # get the hashes of the files
+                md5_file_hash = md5_hash(file_with_path)
+                sha1_file_hash = sha1_hash(file_with_path)
+                # store the data in a dictionary
+                report = (os.path.abspath(dir_path), file_name, file_size, md5_file_hash, sha1_file_hash)
+                cur.execute(insert_query, report)
+        cur.close()
+        conn.close()
+        logger.info("Data inserted successfully.")
+    except (Exception, psycopg2.DatabaseError) as error:
+        logger.exception(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+def table_check(dir_name, db_pw):
+    conn = None
+    try:
+        params = config_open()
+        conn = psycopg2.connect(**params,
+                                password=db_pw,
+                                database='listdirdb')
+        conn.autocommit = True
+        cur = conn.cursor()
+        cur.execute("SELECT to_regclass('public.result');")
+        if cur.fetchone()[0]:
+            logger.info("Table exist... continue with data insertion.")
+            cur.close()
+            db_insert(dir_name, db_pw)
+        else:
+            logger.info("Creating table result....")
+            table = """CREATE TABLE public.result(
+                    id SERIAL NOT NULL PRIMARY KEY,
+                    parent_name varchar NOT NULL,
+                    file_name varchar NOT NULL,
+                    file_size integer NOT NULL,
+                    md5 varchar(32) NOT NULL,
+                    sha1 varchar(40) NOT NULL,
+                    created_at TIMESTAMP NOT NULL DEFAULT NOW())"""
+            cur.execute(table)
+            cur.close()
+            conn.close()
+            db_insert(dir_name, db_pw)
+    except (Exception, psycopg2.DatabaseError) as error:
+        logger.exception(error)
+    finally:
+        if conn is not None:
+            conn.close()
 
 
 def md5_hash(file_to_hash):
@@ -84,28 +209,28 @@ def sha1_hash(file):
 
 def csv_archive(zip_file_name, dir_path):
     # writing files to a zipfile
-    os.chdir(dir_path)
     zip_name = "{}.zip".format(zip_file_name)
     with zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED) as file_to_zip:
         file_to_zip.write(zip_file_name)
     # deleting the csv file
     os.remove(zip_file_name)
+    logger.info("{} created...".format(zip_name))
     return zip_name
 
 
 def csv_add_date_time(csv_file):
     # add date and time to the output
     now = datetime.now()
-    datetime_now = now.strftime("%m-%d-%y %I-%M-%p")
-    csv_filename_now = "{} {}.csv".format(csv_file, datetime_now)
+    datetime_now = now.strftime("%m-%d-%y-%I-%M-%p")
+    csv_filename_now = "{}-{}.csv".format(csv_file, datetime_now)
     return csv_filename_now
 
 
 def json_add_date_time(json_file):
     # add date and time to the output
     now = datetime.now()
-    datetime_now = now.strftime("%m-%d-%y %I-%M-%p")
-    json_filename_now = "{} {}.json".format(json_file, datetime_now)
+    datetime_now = now.strftime("%m-%d-%y-%I-%M-%p")
+    json_filename_now = "{}-{}.json".format(json_file, datetime_now)
     return json_filename_now
 
 
@@ -118,7 +243,6 @@ def csv_creator(dir_name, csv_file_name):
     # create a csv file
     csv_name = csv_add_date_time(csv_file_name)
     logger.info("{} file created...".format(csv_name))
-    os.chdir(dir_name)
     with open(csv_name, "w+", newline='', encoding='utf-8') as csv_file:
         # header for the csv file
         field_header = ['Parent Name', 'File Name', 'File Size', 'MD5', 'SHA-1']
@@ -138,7 +262,7 @@ def csv_creator(dir_name, csv_file_name):
                 # store the data by row in the csv file
                 csv_writer.writerow(report)
     # archive the csv output file
-    logger.info("{} created...".format(csv_archive(csv_name, dir_name)))
+    csv_archive(csv_name, dir_name)
 
 
 def list_dir(dir_name, csv_file_name):
@@ -161,8 +285,11 @@ def json_create(path_name, file_name):
     else:
         logger.info("path exist...")
         try:
+            scanned_files = []
+            reports = {'Scanned': scanned_files}
             new_json_file = json_add_date_time(file_name)
             with open(new_json_file, 'a') as f:
+                logger.info("{} file created...".format(new_json_file))
                 for dir_path, dir_names, file_names in os.walk(parent_path):
                     for file_name in file_names:
                         file_with_path = "{}{}{}".format(dir_path, os.sep, file_name)
@@ -174,8 +301,9 @@ def json_create(path_name, file_name):
                         report = {'Parent Name': os.path.realpath(dir_path), 'File Name': file_name,
                                   'File Size': file_size,
                                   'MD5': md5_file_hash, 'SHA-1': sha1_file_hash}
-                        json.dump(report, f, indent=2)
-            logger.info("{} created...".format(csv_archive(new_json_file, parent_path)))
+                        scanned_files.append(report)
+                json.dump(reports, f, indent=2)
+            csv_archive(new_json_file, parent_path)
         except OSError as e:
             # catch any error and display
             logger.error("Ops, Something went wrong! {}".format(e), exc_info=True)
@@ -188,7 +316,12 @@ def main():
     config = configparser.ConfigParser()
     parser.add_argument("dir", nargs='?', help="The directory you want to access", type=str)
     parser.add_argument("csv_file_name", nargs='?', help="The file name you want for the csv file", type=str)
+    parser.add_argument('-p', '--password', type=Password, help='Password used in database',
+                        default=Password.DEFAULT)
     group.add_argument("-j", "--json", help="Output a json file instead of csv file", action="store_true")
+    group.add_argument("-c", "--csv", help="Output a csv file instead of json file", action="store_true")
+    group.add_argument("-d", "--database", help="Insert the data to a database instead of saving to a file",
+                       action="store_true")
     args = parser.parse_args()
     # when the user put empty arguments
     if args.dir is None and args.csv_file_name is None:
@@ -196,10 +329,12 @@ def main():
         config.read(path + '/setup.ini')
         # using the default arguments from setup.ini
         list_dir(config['arguments']['file path'], config['arguments']['file name'])
-    elif args.json:
+    if args.json:
         json_create(args.dir, args.csv_file_name)
-    else:
+    elif args.csv:
         list_dir(args.dir, args.csv_file_name)
+    elif args.database:
+        db_check(args.dir, args.password)
 
 
 if __name__ == '__main__':
